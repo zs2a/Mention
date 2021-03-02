f6b5ca87346518170f8e70f1ca680c0b9cf13d06
ssenshi <67323109+ssenshi@users.noreply.github.com>
2021-03-02 03:54:54 -0800

    Create executor, register events and commands.

---
+++ src/Executor.java
@@ -0,0 +1,16 @@
+package dev.stan.plugin;
+
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Executor extends JavaPlugin implements Listener {
+	
+	// On enable method, execute as soon as plugin is enabled.
+	@Override
+	public void onEnable() {
+		
+		getLogger().info("Mention Plugin enabled.");
+		getCommand("ping").setExecutor(new PingCommand()); 		// Create new instance of Commands class and point to executor
+		getServer().getPluginManager().registerEvents(new ChatEvents(), this); 		// Create new instance of Events class and point to executor
+	}
+}
