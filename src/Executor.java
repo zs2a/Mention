f3202c5d03eb27fb6212b1c785f642c6f2da6672
dev-stan <67323109+dev-stan@users.noreply.github.com>
2021-03-02 18:49:11 -0800

    Update Executor.java

--- src/Executor.java
+++ src/Executor.java
@@ -1,16 +1,31 @@
 package dev.stan.plugin;
 
+import org.bukkit.ChatColor;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
+
+import dev.stan.plugin.commands.PingToggleCMD;
+
 public class Executor extends JavaPlugin implements Listener {
 	
+	public static String prefix = (ChatColor.DARK_AQUA + "[" + ChatColor.GREEN + "Mention" + ChatColor.DARK_AQUA + "] ");
+	
 	// On enable method, execute as soon as plugin is enabled.
+	
 	@Override
 	public void onEnable() {
 		
 		getLogger().info("Mention Plugin enabled.");
-		getCommand("ping").setExecutor(new PingCommand()); / Create new instance of Commands class and point to executor
-		getServer().getPluginManager().registerEvents(new ChatEvents(), this); // Create new instance of Events class and point to executor
+		// Create new instance of Commands class and point to executor
+		
+		getCommand("ping").setExecutor(new PingToggleCMD());
+		
+		// Create new instance of Events class and point to executor
+		getServer().getPluginManager().registerEvents(new ChatEvents(), this); 
+		
+		
+		
 	}
+
 }
