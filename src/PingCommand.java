94796b67c2aba32ab4dd9e3fce574196202f392b
dev-stan <67323109+dev-stan@users.noreply.github.com>
2021-03-02 18:49:48 -0800

    Update PingCommand.java

--- src/PingCommand.java
+++ src/PingCommand.java
@@ -1,4 +1,4 @@
-package dev.stan.plugin;
+package dev.stan.plugin.commands;
 
 import java.util.HashMap;
 
@@ -7,9 +7,12 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import dev.stan.plugin.Executor;
 import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.TextComponent;
 
-public class PingCommand implements CommandExecutor {
+public class PingToggleCMD implements CommandExecutor {
 
 	public static HashMap<Player, Boolean> enabled = new HashMap<Player, Boolean>();
 
@@ -18,15 +21,15 @@ public class PingCommand implements CommandExecutor {
 		if (enabled.containsKey(player)) {
 			if (enabled.get(player)) {
 				enabled.put(player, false);
-				player.sendMessage(ChatColor.GREEN + "Pings disabled.");
+				player.sendMessage(Executor.prefix + ChatColor.RED + "Pings disabled.");
 			} else {
 				enabled.put(player, true);
-				player.sendMessage(ChatColor.GREEN + "Pings enabled.");
+				player.sendMessage(Executor.prefix + ChatColor.GREEN + "Pings enabled.");
 
 			}
 		} else {
 			enabled.put(player, false); // If you want plugin enabled by default change this value to false.
-			player.sendMessage(ChatColor.GREEN + "Pings disabled.");
+			player.sendMessage(Executor.prefix + ChatColor.RED + "Pings disabled.");
 		}
 
 	}
@@ -35,31 +38,37 @@ public class PingCommand implements CommandExecutor {
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 
 		if (cmd.getName().equalsIgnoreCase("ping")) {
-			
+
 			Player player = (Player) sender;
 			
-			if (player.hasPermission("mention.command"))
-
 			if (sender instanceof Player) {
-
-				togglePluginState(player);
-
-				if (args.length == 1) {
-
+				
+				if (args.length == 0) {
+					
+					if (player.hasPermission("mention.command")) {
+						
+						togglePluginState(player);
+					}
+					
+				}
+				
+				else if (args.length == 1) {
+					
+					if (args[0].equalsIgnoreCase("github")) {
+						
+						TextComponent message = new TextComponent(Executor.prefix + ChatColor.DARK_AQUA + "[" + ChatColor.GREEN + "Click to visit the plugin github page" + ChatColor.DARK_AQUA + "]");
+						message.setClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, "https://github.com/dev-stan/Mention"));
+						
+						player.spigot().sendMessage(message);
+					}
+					
 					if (args[0].equalsIgnoreCase("help")) {
 
-						player.sendMessage(ChatColor.GREEN + "Use the " + ChatColor.DARK_GREEN + "/ping " + ChatColor.GREEN + "command to toggle wether you want to ping and be pinged.");
+						player.sendMessage(Executor.prefix + ChatColor.GREEN + "Use the " + ChatColor.DARK_GREEN + "/ping " + ChatColor.GREEN + "command to toggle between the ability" + ChatColor.GREEN + " to ping (and be pinged). More info on our" + ChatColor.DARK_GREEN + " /ping github" + ChatColor.GREEN + ".");
 					}
 				}
-
-			}
-
-			else {
-
-				System.out.println("You must be in-game to execute this command.");
 			}
 		}
 		return false;
 	}
-
 }
